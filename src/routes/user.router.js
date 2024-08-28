7a0afdd8a713545dbef3953ed3e7d02a93e0efac
Yerko Asbun <asbunyerko01@gmail.com>
2024-08-27 23:13:37 -0400

    a

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,4 +1,4 @@
-const { getAll, create, remove, update, login } = require('../controllers/user.controllers');
+/* const { getAll, create, remove, update, login } = require('../controllers/user.controllers');
 const express = require('express');
 const { verifyJwt } = require('../utils/verifyJWT');
 
@@ -15,5 +15,23 @@ routerUser.route('/login')
 routerUser.route('/:id')
     .delete(verifyJwt, remove)
     .put(verifyJwt, update);
+
+module.exports = routerUser; */
+const { getAll, create, remove, update, login } = require('../controllers/user.controllers');
+const express = require('express');
+const { verifyJwt } = require('../utils/verifyJWT');
+
+const routerUser = express.Router();
+
+routerUser.route('/')
+  .get(verifyJwt, getAll)
+  .post(create);
+
+routerUser.route('/login')
+  .post(login)
+
+routerUser.route('/:id')
+  .delete(verifyJwt, remove)
+  .put(verifyJwt, update);
 
 module.exports = routerUser;
