3ce6684a3629561ed38076a1bf62fa4a62b4725e
Yerko Asbun <asbunyerko01@gmail.com>
2024-08-27 23:24:18 -0400

    mm

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,37 +1,19 @@
-/* const { getAll, create, remove, update, login } = require('../controllers/user.controllers');
+const { getAll, create, remove, update, login } = require('../controllers/user.controllers');
 const express = require('express');
-const { verifyJwt } = require('../utils/verifyJWT');
+const { verifyJWT } = require('../utils/verifyJWT');
 
 
 const routerUser = express.Router();
 
 routerUser.route('/')
-    .get(verifyJwt, getAll)
+    .get(verifyJWT, getAll)
     .post(create);
 
 routerUser.route('/login')
     .post(login)
 
 routerUser.route('/:id')
-    .delete(verifyJwt, remove)
-    .put(verifyJwt, update);
-
-module.exports = routerUser; */
-const { getAll, create, remove, update, login } = require('../controllers/user.controllers');
-const express = require('express');
-const { verifyJwt } = require('../utils/verifyJWT');
-
-const routerUser = express.Router();
-
-routerUser.route('/')
-  .get(verifyJwt, getAll)
-  .post(create);
-
-routerUser.route('/login')
-  .post(login)
-
-routerUser.route('/:id')
-  .delete(verifyJwt, remove)
-  .put(verifyJwt, update);
+    .delete(verifyJWT, remove)
+    .put(verifyJWT, update);
 
 module.exports = routerUser;
